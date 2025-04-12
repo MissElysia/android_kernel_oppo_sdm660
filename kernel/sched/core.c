b4bcf09a1fa32eb24eed79767494a488bc3edaf9
knkj55 <knkj55@proton.me>
2025-04-12 08:18:59 +0000

    kernel/sched/core.c: remove raw_spin_lock_irqsave(&p->pi_lock, flags);
    flags had already removed in commit 4c9b5b2ef181c90448496ef8fa75eb67b44deb44

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2868,7 +2868,6 @@ void wake_up_new_task(struct task_struct *p)
 	struct rq *rq;
 
 	add_new_task_to_grp(p);
-	raw_spin_lock_irqsave(&p->pi_lock, flags);
 	p->state = TASK_RUNNING;
 
 	/* Initialize new task's runnable average */
