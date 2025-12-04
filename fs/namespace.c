c4cfc0a2416129a11de05d4b2c0771618a037c9a
愛莉希雅 <1138745761@qq.com>
2025-12-04 21:11:19 +0800

    patch

--- fs/namespace.c
+++ fs/namespace.c
@@ -1156,7 +1156,7 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	// We keep checking for ksu process only until boot-completed stage is triggered
 	if (!susfs_is_boot_completed_triggered && susfs_is_current_ksu_domain()) {
-		mnt = susfs_alloc_sus_vfsmnt(name ?: "none");
+		mnt = susfs_alloc_sus_vfsmnt(name);
 		atomic64_add(1, &susfs_ksu_mounts);
 		goto bypass_orig_flow;
 	}
