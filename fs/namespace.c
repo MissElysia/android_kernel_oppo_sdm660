6faa056acc36765805415a7acebc65506d9b941b
愛莉希雅 <1138745761@qq.com>
2025-12-07 16:13:43 +0800

    patch

--- fs/namespace.c
+++ fs/namespace.c
@@ -38,7 +38,6 @@ static DEFINE_IDA(susfs_ksu_mnt_group_ida);
 static atomic64_t susfs_ksu_mounts = ATOMIC64_INIT(0);
 
 static int susfs_mnt_group_start = DEFAULT_KSU_MNT_GROUP_ID;
-static int susfs_mnt_id_start = DEFAULT_KSU_MNT_ID;
 
 #define CL_COPY_MNT_NS BIT(25) /* used by copy_mnt_ns() */
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
